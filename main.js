17f57f0fff02c685b189eeb42ffdf060d6abe60d
Cyn3ah <62955916+Cyn3ah@users.noreply.github.com>
2020-06-29 15:17:11 -0500

    Redeux 2

--- main.js
+++ main.js
@@ -1,64 +1,45 @@
 let coin = {
     state: 0,
-    flip: function() {
-
-        return this.state = Math.floor(Math.random() * 2); 
-        /* 1. Randomly set your coin object's "state" property to be either 
-           0 or 1: use "this.state" to access the "state" property on this object.*/
-
-        
+    flip: function () {
+       this.state = Math.floor(Math.random() * 2);
     },
+
     toString: function() {
         if (this.state === 0) {
-        return "Heads";
+            return "Heads";
         } else {
             return "Tails";
         }
-        
-        /* 2. Return the string "Heads" or "Tails", depending on whether
-           "this.state" is 0 or 1. */
     },
+
     toHTML: function() {
         let image = document.createElement('img');
 
         if (this.state === 0) {
-        image.src = "./images/heads.jpeg"
+            image.src = "./images/heads.jpeg"
         } else {
-        image.src = "./images/tails.jpeg"
+            image.src = "./images/tails.jpeg"
         }
-
-        return image
-
-
-        /* 3. Set the properties of this image element to show either face-up
-           or face-down, depending on whether this.state is 0 or 1.*/
-           return document.body.append(image);
+       
+        return image;
     }
 };
-
 
 function display20flips () {
 
     for (let index = 0; index < 20; index++) {
         coin.flip()
-        coin.toString()
-        mainElement.append(coin.toHTML())
+        let showFlip = document.createElement('p')
+        showFlip.append(coin.toString())
+        document.body.append(showFlip)
+       
     }
 }
 
-function display20images ()  {
+function display20images () {
 
     for (let index = 0; index < 20; index++) {
-        let showFlip = document.createElement('p')
         coin.flip()
-        showFlip.append(coin.toString())
-        mainElement.append(coin.toHTML())
+        document.body.append(coin.toHTML())
     }
-
-    //let textbox = document.createElement('p')
-    //textbox.append(coin.toString())
-    //mainElement.append(textbox)
-
-
 }
-
